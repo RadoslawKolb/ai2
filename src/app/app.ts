087193946d546619fef580721f3cc8a46c8e97d1
Radosław Kolb <kr53914>
2025-12-01 11:29:25 +0100

    done

---
+++ src/app/app.ts
@@ -0,0 +1,24 @@
+import { Component, signal } from '@angular/core';
+import {RouterModule, RouterOutlet} from '@angular/router';
+import {Home} from './home/home';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [Home, RouterModule],
+  template: `
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true" />
+      </header>
+      <section class="content">
+        <router-outlet></router-outlet>
+      </section>
+    </main>
+  `,
+  styleUrl: './app.css'
+})
+
+export class App {
+  protected readonly title = signal('homes');
+}
